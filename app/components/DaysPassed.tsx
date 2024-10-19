8ffe8b6a2cee37e0df75c93627be544ae77270c5
dheereshagrwal <da11@iitbbs.ac.in>
2024-10-19 22:24:16 +0530

    removed margin

--- app/components/DaysPassed.tsx
+++ app/components/DaysPassed.tsx
@@ -36,12 +36,12 @@ const DaysPassed: React.FC = () => {
 
   return (
     <div
-      className={`flex flex-col items-center justify-center h-screen bg-zinc-950 p-6 ${inter.className}`}
+      className={`flex flex-col items-center justify-center h-screen bg-zinc-950 gap-6 ${inter.className}`}
     >
       <h1 className="text-3xl md:text-5xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 h-18 md:h-16">
         Days passed since not getting blocked:
       </h1>
-      <span className="text-6xl md:text-8xl font-bold text-white rounded-lg m-6">
+      <span className="text-6xl md:text-8xl font-bold text-white rounded-lg">
         {daysPassed}
       </span>
     </div>
